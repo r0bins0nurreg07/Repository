6d1a02c92d852a6854b1581147eef12b5c90cbaf
Robinson Urrego Goez <95582619+r0bins0nurreg07@users.noreply.github.com>
2025-12-17 14:16:57 -0500

    1

--- script.js
+++ script.js
@@ -21,14 +21,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const p3= document.getElementById("p3");
   
     // const imagecat = [
-    //     "/Image/cat1.png",
-    //     "/Image/cat2.png",
-    //     "/Image/cat3.png"
+    //     "./Image/cat1.png",
+    //     "./Image/cat2.png",
+    //     "./Image/cat3.png"
     // ];
     // const imagedog= [
-    //     "/Image/dogboxer.png",
-    //     "/Image/dogdoberman.png",
-    //     "/Image/dogpastor.png"
+    //     "./Image/dogboxer.png",
+    //     "./Image/dogdoberman.png",
+    //     "./Image/dogpastor.png"
 
     // ];
     const imagecat = [
